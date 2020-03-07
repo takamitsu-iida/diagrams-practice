ed2b710644535f65772b9df3d1fae6f0a3a1f907
takamitsu-iida <takamitsu.iida@gmail.com>
2020-03-07 18:13:12 +0900

    [ci skip]

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ brew install graphviz
 ```bash
 pip install diagrams
 ```
+
+## 接続のオペレーター
+
+使えるオペレータは `>>`, `<<`, `-` の3個。
+
+`>>` は左から右への接続、`<<`は逆に右から左への接続、`-`は向きのない接続。
+
+```python
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+from diagrams.aws.storage import S3
+
+with Diagram("Web Services", show=False):
+    ELB("lb") >> EC2("web") >> RDS("userdb") >> S3("store")
+    ELB("lb") >> EC2("web") >> RDS("userdb") << EC2("stat")
+    (ELB("lb") >> EC2("web")) - EC2("web") >> RDS("userdb")
+```
+
+定義した順番と逆の順番に描画される。
+
+`>>` と `-` を同時に使うときは演算子の強度に注意が必要。予想外の動きをするときは()を使って調整する。
+
+## 向き
+
+directionには`TB`, `BT`, `LR`, `RL`を指定する。デフォルトは`LR`で、左から右に向かって描画する。
--- bin/main.py
+++ bin/main.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned
+# pylint: disable=missing-docstring
+# pylint: disable=pointless-statement
+# pylint: disable=protected-access
 
-#
-# 標準ライブラリのインポート
-#
-import argparse
 import logging
 import os
 import sys
 
 try:
   from diagrams import Diagram
+  from diagrams import Cluster
   from diagrams.aws.compute import EC2
+  from diagrams.aws.compute import ECS
+  from diagrams.aws.compute import EKS
+  from diagrams.aws.compute import Lambda
   from diagrams.aws.database import RDS
   from diagrams.aws.network import ELB
+  from diagrams.aws.network import Route53
+  from diagrams.aws.database import Redshift
+  from diagrams.aws.integration import SQS
+  from diagrams.aws.storage import S3
+
 except ImportError as e:
   logging.exception(e)
   sys.exit(-1)
@@ -44,6 +53,55 @@ if not here("../lib/site-packages") in sys.path:
 
 if __name__ == '__main__':
 
+  def test1():
+    with Diagram("Workers", show=False, direction="TB", filename=sys._getframe().f_code.co_name):
+      lb = ELB("lb")
+      db = RDS("events")
+      lb >> EC2("worker1") >> db
+      lb >> EC2("worker2") >> db
+      lb >> EC2("worker3") >> db
+      lb >> EC2("worker4") >> db
+      lb >> EC2("worker5") >> db
+
+  def test2():
+    with Diagram("Grouped Workers", show=False, direction="TB", filename=sys._getframe().f_code.co_name):
+      ELB("lb") >> [EC2("worker1"),
+                    EC2("worker2"),
+                    EC2("worker3"),
+                    EC2("worker4"),
+                    EC2("worker5")] >> RDS("events")
+
+  def test3():
+    with Diagram("Simple Web Service with DB Cluster", show=False, filename=sys._getframe().f_code.co_name):
+      dns = Route53("dns")
+      web = ECS("service")
+
+      with Cluster("DB Cluster"):
+        db_master = RDS("master")
+        db_master - [RDS("slave1"), RDS("slave2")]
+
+      dns >> web >> db_master
+
+  def test4():
+    with Diagram("Event Processing", show=False, filename=sys._getframe().f_code.co_name):
+      source = EKS("k8s source")
+
+      with Cluster("Event Flows"):
+        with Cluster("Event Workers"):
+          workers = [ECS("worker1"), ECS("worker2"), ECS("worker3")]
+
+        queue = SQS("event queue")
+
+        with Cluster("Processing"):
+          handlers = [Lambda("proc1"), Lambda("proc2"), Lambda("proc3")]
+
+      store = S3("events store")
+      dw = Redshift("analytics")
+
+      source >> workers >> queue >> handlers
+      handlers >> store
+      handlers >> dw
+
 
   def main():
     """メイン関数
@@ -52,16 +110,11 @@ if __name__ == '__main__':
       int -- 正常終了は0、異常時はそれ以外を返却
     """
 
-    # 引数処理
-    parser = argparse.ArgumentParser(description='main script.')
-    parser.add_argument('-d', '--dump', action='store_true', default=False, help='Dump')
-    args = parser.parse_args()
+    test1()
+    test2()
+    test3()
+    test4()
 
-    if args.dump:
-      dump()
-
-    with Diagram("Web Service", show=False):
-      ELB("lb") >> EC2("web") >> RDS("userdb")
 
     return 0
 
